34e9519b3ab15a65a593422ae396a273c730b975
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-05-31 19:31:40 +0100

    Update 0-positive_or_negative.py

--- 0x01-python-if_esle_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_esle_loops_functions/0-positive_or_negative.py
@@ -1,1 +1,9 @@
-bin
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+if number > 0:
+    print(f"{:d} is positive")
+elif number == 0:
+    print(f"{:d} is zero")
+else:
+    print(f"{:d} is negative")
