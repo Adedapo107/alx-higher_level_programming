2a90bcdd76fe9c69668988dbeec4235d557635f4
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-06-20 19:57:25 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,5 @@ def safe_print_division(a, b):
         print("Inside result: {:.1f}".format(result))
     except:
         result = None
-        print("Inside result: {}".format(result))
     finally:
         print("Inside result: {}".format(result))
