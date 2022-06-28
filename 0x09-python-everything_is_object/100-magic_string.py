90aa95a930c79f74cccc54a478b0599f5b5f786d
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-06-28 19:05:25 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 
-def call_counter(func):
-    def helper():
-        helper.calls += 1
-        return func()
-    helper.calls = 0
-
-    return helper
-
-
-@call_counter
 def magic_string():
-    return 'BestSchool, ' * (magic_string.calls - 1) + 'BestSchool'
+    magic_string.counter = getattr(magic_string, "counter", 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.counter)])
