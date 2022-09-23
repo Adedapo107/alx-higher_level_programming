332f379b15656f39747a998af649066e41b07a4c
Abdulmaleek Olaifa <100152296+Adedapo107@users.noreply.github.com>
2022-09-23 21:57:45 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -7,9 +7,8 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print("{}".format(r.text))
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    status = r.status_code
+    print(r.text) if status < 400 else print(
+        "Error code: {}".format(r.status_code))
